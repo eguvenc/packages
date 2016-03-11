1d74477faeacd3874645a15171398a5fe214bf55
eguvenc <eguvenc@gmail.com>
2016-03-11 13:10:13 +0200

    updates

---
+++ Obullo/Validator/Rules/AlnumDashUnicode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Obullo\Validator\Rules;
+
+use Obullo\Validator\FieldInterface as Field;
+
+/**
+ * Alnum dash unicode (https://github.com/vlucas/valitron/issues/79)
+ * 
+ * @copyright 2009-2016 Obullo
+ * @license   http://opensource.org/licenses/MIT MIT license
+ */
+class AlnumDashUnicode
+{
+    /**
+     * Call next
+     * 
+     * @param Field    $field object
+     * @param Callable $next  object
+     * 
+     * @return object
+     */
+    public function __invoke(Field $field, Callable $next)
+    {
+        if (preg_match("/^[\s0-9_\-\pL]+$/u", $field->getValue())) {
+
+            return $next($field);
+        }
+        return false;
+    }
+}
---
+++ Obullo/Validator/Rules/AlphaUnicode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Obullo\Validator\Rules;
+
+use Obullo\Validator\FieldInterface as Field;
+
+/**
+ * Alpha Unicode (https://github.com/vlucas/valitron/issues/79)
+ * 
+ * @copyright 2009-2016 Obullo
+ * @license   http://opensource.org/licenses/MIT MIT license
+ */
+class AlphaUnicode
+{
+    /**
+     * Call next
+     * 
+     * @param Field    $field object
+     * @param Callable $next  object
+     * 
+     * @return object
+     */
+    public function __invoke(Field $field, Callable $next)
+    {
+        if (preg_match("/^[\s\pL]+$/u", $field->getValue())) {
+
+            return $next($field);
+        }
+        return false;
+    }
+}
